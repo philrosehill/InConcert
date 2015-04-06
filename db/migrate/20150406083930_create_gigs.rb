37e545a6c188a5427e007b5982c282b2b862e5f1
philrosehill <philrosehill@gmail.com>
2015-04-06 04:57:11 -0400

    Models generated & migrated, Controllers generated

--- db/migrate/20150406083930_create_gigs.rb
+++ db/migrate/20150406083930_create_gigs.rb
@@ -1,7 +1,7 @@
 class CreateGigs < ActiveRecord::Migration
   def change
     create_table :gigs do |t|
-      t.interger :performer_id
+      t.integer :performer_id
       t.integer :venue_id
 
       t.timestamps null: false
